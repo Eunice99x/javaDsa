094314f9c01616d4d35a41f73451956cbb4cc00f
YounesOut <younesouterbah1@gmail.com>
2025-02-12 20:18:31 +0100

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,36 @@
 import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    int[] arr = {5, 3, 8, 4, 2, 7};
-    mergeSort(arr);
-    System.out.println(Arrays.toString(arr));
-}
+    public static void main(String[] args) {
+      int[] arr = {5, 3, 8, 4, 2, 7};
+      quickSort(arr, 0, arr.length - 1);
+      System.out.println(Arrays.toString(arr));
+  }
 
-public static void mergeSort(int[] arr) {
-    if (arr.length < 2) return;
+  public static void quickSort(int[] arr, int low, int high) {
+      if (low < high) {
+          int pivotIndex = partition(arr, low, high);
+          quickSort(arr, low, pivotIndex - 1);
+          quickSort(arr, pivotIndex + 1, high);
+      }
+  }
 
-    int mid = arr.length / 2;
-    int[] left = Arrays.copyOfRange(arr, 0, mid);
-    int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+  private static int partition(int[] arr, int low, int high) {
+      int pivot = arr[high];
+      int i = low - 1;
+      for (int j = low; j < high; j++) {
+          if (arr[j] < pivot) {
+              i++;
+              swap(arr, i, j);
+          }
+      }
+      swap(arr, i + 1, high);
+      return i + 1;
+  }
 
-    mergeSort(left);
-    mergeSort(right);
-    merge(arr, left, right);
-}
-
-private static void merge(int[] arr, int[] left, int[] right) {
-    int i = 0, j = 0, k = 0;
-
-    while (i < left.length && j < right.length) {
-        arr[k++] = left[i] < right[j] ? left[i++] : right[j++];
-    }
-
-    while (i < left.length) arr[k++] = left[i++];
-    while (j < right.length) arr[k++] = right[j++];
-}
+  private static void swap(int[] arr, int i, int j) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+  }
 }
