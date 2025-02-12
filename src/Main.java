ca9a98e39f8817c5a13d394cb785ed0b28ce31ab
YounesOut <younesouterbah1@gmail.com>
2025-02-12 20:17:23 +0100

    merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
-      int[] arr = {5, 3, 8, 4, 2, 7};
-      for(int i = 1; i < arr.length; i++){
-        int tmp = arr[i];
-        int j = i - 1;
+  public static void main(String[] args) {
+    int[] arr = {5, 3, 8, 4, 2, 7};
+    mergeSort(arr);
+    System.out.println(Arrays.toString(arr));
+}
 
-        while(j >= 0 && arr[j] > tmp){
-          arr[j+1] = arr[j];
-          j--;
-        }
-        arr[j+1] = tmp;
-      }; 
+public static void mergeSort(int[] arr) {
+    if (arr.length < 2) return;
 
-      System.out.println(Arrays.toString(arr));
+    int mid = arr.length / 2;
+    int[] left = Arrays.copyOfRange(arr, 0, mid);
+    int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+    mergeSort(left);
+    mergeSort(right);
+    merge(arr, left, right);
+}
+
+private static void merge(int[] arr, int[] left, int[] right) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < left.length && j < right.length) {
+        arr[k++] = left[i] < right[j] ? left[i++] : right[j++];
     }
+
+    while (i < left.length) arr[k++] = left[i++];
+    while (j < right.length) arr[k++] = right[j++];
+}
 }
